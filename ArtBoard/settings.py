3f558cf7881545407b32c25843480beea9bb3066
Alensainkaz <alen232kz@gamil.com>
2025-11-24 16:33:36 +0600

    Fix STATIC_ROOT import for Render deployment

--- ArtBoard/settings.py
+++ ArtBoard/settings.py
@@ -122,11 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+import os
 AUTH_USER_MODEL = 'users.User'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-import os
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
